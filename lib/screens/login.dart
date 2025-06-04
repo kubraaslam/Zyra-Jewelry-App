fd6cd9334caf63e62f1fdfc1ecc89312f8b12e0b
Kubra Aslam <cb012428@students.apiit.lk>
2025-06-04 12:02:53 +0530

    editing the landscape mode

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,133 +9,146 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Logo
-              Image.asset('assets/images/logo.png', height: 100),
-              SizedBox(height: 30),
-              // Login Title
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontFamily: 'PlayfairDisplay',
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Welcome back! Please login to your account',
-                style: TextStyle(
-                  fontFamily: 'PlayfairDisplay',
-                  fontWeight: FontWeight.w500,
-                  color: Colors.grey[700],
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 30),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 30), // Top spacing
+                // Logo
+                Image.asset('assets/images/logo.png', height: 100),
+                SizedBox(height: 30),
 
-              // Email Input
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Email:',
-                  style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500),
-                ),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
+                // Login Title
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontFamily: 'PlayfairDisplay',
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-
-              // Password Input
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Password:',
-                  style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500),
-                ),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
+                SizedBox(height: 10),
+                Text(
+                  'Welcome back! Please login to your account',
+                  style: TextStyle(
+                    fontFamily: 'PlayfairDisplay',
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[700],
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(height: 30),
+                SizedBox(height: 30),
 
-              // Login Button
-              SizedBox(
-                width: 200,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Home()),
-                      );
-                  },
+                // Email Input
+                Align(
+                  alignment: Alignment.centerLeft,
                   child: Text(
-                    'Login',
+                    'Email:',
                     style: TextStyle(
                       fontFamily: 'PlayfairDisplay',
-                      fontSize: 18,
-                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-
-              // Sign up Link
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'New User?',
-                    style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500),
+                SizedBox(height: 5),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
-                  TextButton(
+                ),
+                SizedBox(height: 20),
+
+                // Password Input
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Password:',
+                    style: TextStyle(
+                      fontFamily: 'PlayfairDisplay',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                // Login Button
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Signup()),
+                        MaterialPageRoute(builder: (context) => Home()),
                       );
                     },
                     child: Text(
-                      'Sign up',
+                      'Login',
                       style: TextStyle(
                         fontFamily: 'PlayfairDisplay',
-                        fontWeight: FontWeight.w500,
-                        color: Colors.indigo,
-                        decoration: TextDecoration.underline,
+                        fontSize: 18,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 20),
+
+                // Sign up Link
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'New User?',
+                      style: TextStyle(
+                        fontFamily: 'PlayfairDisplay',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Signup()),
+                        );
+                      },
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontFamily: 'PlayfairDisplay',
+                          fontWeight: FontWeight.w500,
+                          color: Colors.indigo,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40), // Bottom padding
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -8,125 +8,108 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Logo
-              Image.asset('assets/images/logo.png', height: 100),
-              SizedBox(height: 30),
-              // Signup Title
-              Text(
-                'Signup',
-                style: TextStyle(
-                  fontFamily: 'PlayfairDisplay',
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(
+              children: [
+                // Logo
+                Image.asset('assets/images/logo.png', height: 100),
+                SizedBox(height: 30),
 
-              // Username Input
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Username:',
-                  style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500),
-                ),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
+                // Signup Title
+                Text(
+                  'Signup',
+                  style: TextStyle(
+                    fontFamily: 'PlayfairDisplay',
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
+                SizedBox(height: 30),
 
-              // Email Input
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Email:',
-                  style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500),
-                ),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Password Input
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Password:',
-                  style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500,),
-                ),
-              ),
-              SizedBox(height: 5),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-
-              // Signup Button
-              SizedBox(
-                width: 200,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Login()),
-                      );
-                  },
+                // Username Input
+                Align(
+                  alignment: Alignment.centerLeft,
                   child: Text(
-                    'Create Account',
+                    'Username:',
                     style: TextStyle(
                       fontFamily: 'PlayfairDisplay',
-                      fontSize: 18,
-                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-
-              // Login Link
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Already have an account?',
-                    style: TextStyle(fontFamily: 'PlayfairDisplay',fontWeight: FontWeight.w500,),
+                SizedBox(height: 5),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
-                  TextButton(
+                ),
+                SizedBox(height: 20),
+
+                // Email Input
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Email:',
+                    style: TextStyle(
+                      fontFamily: 'PlayfairDisplay',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // Password Input
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Password:',
+                    style: TextStyle(
+                      fontFamily: 'PlayfairDisplay',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                // Signup Button
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -134,17 +117,48 @@ class Signup extends StatelessWidget {
                       );
                     },
                     child: Text(
-                      'Login',
+                      'Create Account',
                       style: TextStyle(
                         fontFamily: 'PlayfairDisplay',
-                        decoration: TextDecoration.underline,
-                        color: Colors.indigo,
+                        fontSize: 18,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 20),
+
+                // Login Link
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(
+                        fontFamily: 'PlayfairDisplay',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Login()),
+                        );
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontFamily: 'PlayfairDisplay',
+                          decoration: TextDecoration.underline,
+                          color: Colors.indigo,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
