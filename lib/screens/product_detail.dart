3b770cf31eea8e9bd338670ea645971d88e382db
Kubra Aslam <cb012428@students.apiit.lk>
2025-06-08 18:20:56 +0530

    ui edits

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -3,57 +3,45 @@ import 'package:jewelry_store/models/product_data.dart';
 
 class ProductDetail extends StatefulWidget {
   final Product item;
+  final List<Product> cart; // Shared cart list
 
-  const ProductDetail({super.key, required this.item});
+  const ProductDetail({super.key, required this.item, required this.cart});
 
   @override
   State<ProductDetail> createState() => _ProductDetailState();
 }
 
 class _ProductDetailState extends State<ProductDetail> {
-  final List<Product> cart = [];
-
   int quantity = 1;
 
-  void increaseQuantity() {
-    setState(() {
-      quantity++;
-    });
-  }
-
-  void decreaseQuantity() {
-    if (quantity > 1) {
-      setState(() {
-        quantity--;
-      });
-    }
-  }
-
   void addToCart(Product product) {
-    setState(() {
-      final index = cart.indexWhere((item) => item.title == product.title);
+    final index = widget.cart.indexWhere((item) => item.title == product.title);
 
-      if (index != -1) {
-        cart[index].quantity++;
-      } else {
-        cart.add(Product(
+    if (index != -1) {
+      // If product is already in the cart, increase quantity
+      widget.cart[index].quantity += quantity;
+    } else {
+      // Else add as new product
+      widget.cart.add(
+        Product(
           title: product.title,
           type: product.type,
           price: product.price,
           image: product.image,
           description: product.description,
-          quantity: 1,
-        ));
-      }
-    });
+          quantity: quantity,
+        ),
+      );
+    }
 
+    // Show snackbar
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(
           '${product.title} added to cart!',
-          style: TextStyle(fontFamily: 'Roboto'),
+          style: TextStyle(fontSize: 16),
         ),
-        backgroundColor: const Color.fromARGB(255, 67, 68, 67),
+        backgroundColor: Colors.black,
         duration: Duration(seconds: 2),
       ),
     );
@@ -65,72 +53,106 @@ class _ProductDetailState extends State<ProductDetail> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(item.title),
-        titleTextStyle: TextStyle(fontFamily: 'Roboto'),
+        backgroundColor: Colors.white,
+        title: Text(
+          item.title,
+          style: TextStyle(color: Colors.black, fontFamily: 'Roboto'),
+        ),
+        iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset(item.image, height: 200),
-            SizedBox(height: 16),
+            Image.asset(item.image, height: 300, fit: BoxFit.contain),
+            SizedBox(height: 20),
             Text(
               item.title,
               style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 20,
+                fontSize: 22,
                 fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
               ),
+              textAlign: TextAlign.center,
             ),
-            Text(item.type, style: TextStyle(color: Colors.grey, fontFamily: 'Roboto')),
-            SizedBox(height: 16),
             Text(
-              'Price: LKR ${item.price.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 18, fontFamily: 'Roboto'),
+              item.type,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.grey,
+                fontSize: 14,
+                fontFamily: 'Roboto',
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            Text(
+              'LKR ${item.price.toStringAsFixed(2)}',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black87,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+              ),
+              textAlign: TextAlign.center,
             ),
             SizedBox(height: 20),
-            Text(
-              item.description,
-              style: TextStyle(fontSize: 14),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Text(
+                item.description,
+                style: TextStyle(fontSize: 14, fontFamily: 'Roboto'),
+                textAlign: TextAlign.center,
+              ),
             ),
-            Spacer(),
+            SizedBox(height: 30),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment:
+                  MainAxisAlignment
+                      .center, // Center the row children horizontally
               children: [
                 IconButton(
-                  icon: Icon(Icons.remove, color: Colors.white),
-                  onPressed: decreaseQuantity,
-                  style: IconButton.styleFrom(
-                    backgroundColor: Colors.grey[800],
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                  ),
+                  icon: Icon(Icons.remove_circle_outline, size: 30),
+                  onPressed: () {
+                    if (quantity > 1) {
+                      setState(() {
+                        quantity--;
+                      });
+                    }
+                  },
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(
-                    '$quantity',
-                    style: TextStyle(fontSize: 18),
-                  ),
+                Text(
+                  quantity.toString(),
+                  style: TextStyle(fontSize: 20, fontFamily: 'Roboto'),
                 ),
                 IconButton(
-                  icon: Icon(Icons.add, color: Colors.white),
-                  onPressed: increaseQuantity,
-                  style: IconButton.styleFrom(
-                    backgroundColor: Colors.grey[800],
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                  ),
+                  icon: Icon(Icons.add_circle_outline, size: 30),
+                  onPressed: () {
+                    setState(() {
+                      quantity++;
+                    });
+                  },
                 ),
               ],
             ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => addToCart(item),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+            SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                onPressed: () => addToCart(item),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  textStyle: TextStyle(fontSize: 16, fontFamily: 'Roboto'),
+                ),
+                child: Text('Add to Cart'),
               ),
-              child: Text('Add to Cart'),
             ),
           ],
         ),
